a73322b3d5210befa371913973f322e34628997c
Carlos Urquiza <webquiza@gmail.com>
2021-02-13 22:39:53 -0800

    add UI logic and initial business logic function with first if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
 // Business logic
 
+function roboTalk(input) {
+  let userNumber = [];
+  for (let i = 0; i <= input; i++) {
+    userNumber.push(i.toString());
+
+    if (userNumber[i].includes("3")) {
+      userNumber[i] = " Won't you be my neighbor?";
+    } 
+  }
+  return userNumber;
+}
+
 
 // User interface logic 
 
 $(document).ready(function() {
-  
+  $("form#intakeForm").submit(function(event) {
+    event.preventDefault();
+
+    let number = parseInt($("#input").val());
+
+    const talk = roboTalk(number);
+
+    $("#userResults").text(talk);
+  });
 });
